b8374c249e63e07d8a1352ef88dc15148a96386a
Armstrong Olusoji <armstrongolusoji9@gmail.com>
2025-04-08 10:16:39 +0400

    Fixed deployment issue

--- subarashi/app/test/route.js
+++
@@ -1,8 +0,0 @@
-// app/test/route.js
-import { NextResponse } from "next/server";
-
-export const GET = async () => {
-  return new NextResponse("Hello from test route", {
-    status: 200,
-  });
-};
